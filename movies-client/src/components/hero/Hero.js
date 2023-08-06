7317be04cde9546097c1897207086630f9ede8f8
Morandev <moranfcnddev@gmail.com>
2023-08-06 20:27:31 -0300

    add: carousel styles

--- movies-client/src/components/hero/Hero.js
+++ movies-client/src/components/hero/Hero.js
@@ -3,18 +3,18 @@ import React from 'react'
 import Carousel from 'react-material-ui-carousel'
 import { Paper } from '@mui/material'
 
-import styles from './Hero.css'
+import './Hero.css'
 
 const Hero = ({ moviesArr = [] }) => {
 
     return (
-        <div>
+        <div className='movie-carousel-container'>
             <Carousel>
                 {
                     moviesArr.map(m =>
                         <Paper key={m.imdb} >
                             <div className='movie-card-container'>
-                                <div className='movie-card'>
+                                <div className='movie-card' style={{"--img": `url(${m.backdrops[0]})`}}>
                                     <div className='movie-detail'>
                                         <div className='movie-poster'>
                                             <img src={m.poster} alt='' />

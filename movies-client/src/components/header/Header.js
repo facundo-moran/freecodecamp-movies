64130d693d68d597815a0e5ad71c5eca07c9ce06
Morandev <moranfcnddev@gmail.com>
2023-08-07 21:51:46 -0300

    refactor: link redirections

--- movies-client/src/components/header/Header.js
+++ movies-client/src/components/header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
     return (
         <Navbar bg='dark' variant='dark' expand='lg'>
             <Container fluid>
-                <Navbar.Brand href='/' style={{ "color": 'gold' }}>
+                <Navbar.Brand href='/freecodecamp-movies' style={{ "color": 'gold' }}>
                     <FontAwesomeIcon icon={faVideoSlash} />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls='navbarScroll' />
@@ -18,8 +18,8 @@ const Header = () => {
                         style={{ maxHeight: '100px' }}
                         navbarScroll
                     >
-                        <NavLink className="nav-link" to="/">Home</NavLink>
-                        <NavLink className="nav-link" to="/watchList">Watch List</NavLink>
+                        <NavLink className="nav-link" to="/freecodecamp-movies">Home</NavLink>
+                        <NavLink className="nav-link" to="/freecodecamp-movies/watchList">Watch List</NavLink>
                     </Nav>
                     <Button variant='outline-info' className='me-2'>Login</Button>
                     <Button variant='outline-info' className='me-2'>Register</Button>
